5af5565cb2c0d3f9b89e20acde11e42d0ac31516
michael irvin <michael.w.irvin@vanderbilt.edu>
2014-06-16 16:29:13 -0500

    update

--- anrm/numtools/get_hessian_1_0.py
+++ anrm/numtools/get_hessian_1_0.py
@@ -13,13 +13,13 @@ import matplotlib.pyplot as plt
 from anrm.irvin_mod_v5_tester import model
 
 #----Experiment Name--------
-Exp_name = ('CompII_Hyp_123_Apop2')
+Exp_name = ('CompII_Hyp_123_Necr1')
 print Exp_name
 
 #----Data and conditions----
 #init_conc = {'Apop1':{'TNFa_0': 600}}
-init_conc = {'Apop2':{'TNFa_0': 1200}}
-#init_conc = {'Necr1':{'TNFa_0':1800, 'zVad_0':9.6e6, 'FADD_0':0}}
+#init_conc = {'Apop2':{'TNFa_0': 1200}}
+init_conc = {'Necr1':{'TNFa_0':1800, 'zVad_0':9.6e6, 'FADD_0':0}}
 
 #init_conc = {'Apop2':{'TNFa_0': 1200}, 'Necr1':{'TNFa_0':1800, 'zVad_0':9.6e6, 'FADD_0':0}}
 #init_conc = {'Apop1':{'TNFa_0': 600}, 'Apop2':{'TNFa_0': 1200}}
